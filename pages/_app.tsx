be1deaa3e4e1fdb682b58f443091bfe39e127caa
Daniel McGrane <daniel.mcgrane@zebedee.io>
2022-10-26 17:04:36 +0100

    swap out mixpanel for simple analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import mixpanel from "mixpanel-browser";
-
-mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN ?? "");
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+    </>
+  );
 }
 
 export default MyApp;
