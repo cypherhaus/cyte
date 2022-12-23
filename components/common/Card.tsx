cad78299d8cb92d738550141428e3de74ccda6db
Daniel McGrane <daniel.mcgrane@zebedee.io>
2022-12-23 09:45:15 +0000

    fix on mobile

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -10,12 +10,14 @@ interface CardProps {
 export const Card = ({ title, description, link }: CardProps) => (
   <Link href={link}>
     <div className="flex flex-row cursor-pointer">
-      <div className="flex justify-center items-center shrink-0 p-4 h-40 w-[180px] h-[180px] max-w-[280px] bg-lsd cursor-pointer z-10">
+      <div className="min-w-[200px] w-[200px] bg-lsd h-[200px] max-h-[200px]">
         {/* <Image src={logo} alt="Test" /> */}
       </div>
-      <div className="w-[400px]  p-4 border-solid border border-black">
+      <div className="max-w-[300px]  max-h-[200px] p-4 border-solid border border-black">
         <p className="text-2xl font-bold mb-4 cursor-pointer">{title}</p>
-        <p className="text-left text-xl font-normal leading-8">{description}</p>
+        <p className="text-left desktop:text-xl font-normal leading-8">
+          {description}
+        </p>
       </div>
     </div>
   </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      desktop: "800px",
+    },
     extend: {
       fontFamily: {
         display: ["Beyond"],
