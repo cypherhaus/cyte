9bb6bf1bbd00927e81b41f277d2dbe504da57fe4
Daniel McGrane <daniel.mcgrane@zebedee.io>
2023-04-02 19:49:15 +0100

    updated blog

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -12,10 +12,7 @@ interface CardProps {
 export const Card = ({ title, description, link }: CardProps) => (
   <Link href={link}>
     <div className="flex flex-row cursor-pointer">
-      <div className="min-w-[200px] w-[200px] h-[200px] max-h-[200px]">
-        <Image src={orangeSunshine} alt="Test" />
-      </div>
-      <div className="max-w-[300px]  max-h-[200px] p-4 border-solid border border-black">
+      <div className="max-w-[500px]  max-h-[200px] pb-4 border-b-solid border-b-2 border-black">
         <p className="text-2xl font-bold mb-4 cursor-pointer">{title}</p>
         <p className="text-left desktop:text-xl font-normal leading-8">
           {description}
