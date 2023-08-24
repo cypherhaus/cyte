d01356c79a6d223c23f11b037149ab3787ce3b4f
Daniel McGrane <dr.mcgrane@gmail.com>
2023-08-24 20:35:35 +0100

    added anlaytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
-      <noscript>
-        <img
-          src="https://queue.simpleanalyticscdn.com/noscript.gif"
-          alt=""
-          referrerPolicy="no-referrer-when-downgrade"
-        />
-      </noscript>
+      <Analytics />
     </>
   );
 }
